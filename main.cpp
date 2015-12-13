a650cf51af564e47322a92a63a1117c57e099da0
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-13 17:08:25 -0500

    Fixes a bug in the prototype of filterFiles which caused a linker error
    -	main now tests the filterFiles function.

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using std::cout;
 
 
 void outputProperties (vector<File*>& files);
-vector<File*> filterFiles (vector<File*>& files, std::string extension);
+vector<File*> filterFiles (vector<File*> files, std::string extension);
 void filterFilesRec (vector<File*>& sourceVector, vector<File*>& destVector, std::string extension);
 
 int main() {
@@ -21,7 +21,13 @@ int main() {
 	vecOfFiles.push_back (new ImageFile ("Evan's First File", 10, 10, 16));
 	vecOfFiles.push_back (new TextFile ("Evan's Second File"));
 	vecOfFiles.push_back (new ImageFile ("Evan's Third File", 20, 10, 32));
+	
+	cout << "Testing outputProperties():\n";
 	outputProperties (vecOfFiles);
+
+	cout << "\n\nTesting filterFiles():\n";
+	vector<File*> testingFilter = filterFiles (vecOfFiles, "txt");
+	outputProperties (testingFilter);
 	return 0;
 }
 
