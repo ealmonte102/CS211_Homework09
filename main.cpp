9d891fc5a493d14142fe711d2c0cd1f1ac2a58a8
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-14 01:33:06 -0500

    Updates main.cpp's outputProperties and filterFiles to recent versions
    -	Updated based on changes made in Project_2.

--- main.cpp
+++ main.cpp
@@ -11,50 +11,61 @@
 using std::vector;
 using std::cout;
 
-
-void outputProperties (vector<File*>& files);
-vector<File*> filterFiles (vector<File*> files, std::string extension);
+vector<File*> filterFiles (const vector<File*>& files, std::string extension);
 void filterFilesRec (vector<File*>& sourceVector, vector<File*>& destVector, std::string extension);
+void outputProperties (const vector<File*>& files);
+void outputPropertiesRec (vector<File*>& files);
 
-int main() {
+int main( ) {
 	vector<File*> vecOfFiles;
-	vecOfFiles.push_back (new ImageFile ("Evan's First File", 10, 10, 16));
-	vecOfFiles.push_back (new TextFile ("Evan's Second File"));
-	vecOfFiles.push_back (new ImageFile ("Evan's Third File", 20, 10, 32));
-	
+	vecOfFiles.push_back(new ImageFile("Evan's First File", 10, 10, 16));
+	vecOfFiles.push_back(new TextFile("Evan's Second File"));
+	vecOfFiles.push_back(new ImageFile("Evan's Third File", 20, 10, 32));
+
 	cout << "Testing outputProperties():\n";
-	outputProperties (vecOfFiles);
+	outputProperties(vecOfFiles);
 
 	cout << "\n\nTesting filterFiles():\n";
-	vector<File*> testingFilter = filterFiles (vecOfFiles, "txt");
-	outputProperties (testingFilter);
+	vector<File*> testingFilter = filterFiles(vecOfFiles, "txt");
+	outputProperties(testingFilter);
 	return 0;
 }
 
-void outputProperties(vector<File*>& files) {
-	if(files.size() != 0) {
-		File* lastElement = files.back ( );
-		files.pop_back ( );
-		outputProperties (files);
-		files.push_back (lastElement);
-		cout << lastElement->getName ( ) << "\n";
-		cout << lastElement->getExtension ( ) << "\n";
-		cout << lastElement->getSize ( ) << "\n";
+void outputProperties(const vector<File*>& files) {
+	vector<File*> tempFiles(files);
+	outputPropertiesRec(tempFiles);
+}
+
+void outputPropertiesRec(vector<File*>& files) {
+	if (files.size() != 0) {
+		File* lastElement = files.back();
+		files.pop_back();
+		outputProperties(files);
+		cout << "Type: " + lastElement->getExtension() + "\n";
+		cout << "Name: " + lastElement->getName() + "\n";
+		if (lastElement->getExtension() == "gif") {
+			ImageFile* anImageFile = dynamic_cast<ImageFile*>(lastElement);
+			cout << "Dimension: " << anImageFile->getDimensionHeight() << " x "
+					<< anImageFile->getDimensionWidth() << "\n";
+		}
+		cout << "Size: " << lastElement->getSize() << " bytes\n\n";
 	}
 }
 
-vector<File*> filterFiles(vector<File*> files, std::string extension) {
-	vector<File*> filesToReturn;
-	filterFilesRec (files, filesToReturn, extension);
+vector<File*> filterFiles(const vector<File*>& files, std::string extension) {
+	vector<File*> tempFiles(files), filesToReturn;
+	filterFilesRec(tempFiles, filesToReturn, extension);
 	return filesToReturn;
 }
 
 void filterFilesRec(vector<File*>& sourceVector, vector<File*>& destVector, std::string extension) {
-	if (sourceVector.size ( ) == 0) { return; }
-	File* fileAtEnd = sourceVector.back ( );
-	sourceVector.pop_back ( );
-	filterFilesRec (sourceVector, destVector, extension);
-	if(fileAtEnd->getExtension() == extension) {
-		destVector.push_back (fileAtEnd);
+	if (sourceVector.size() == 0) {
+		return;
+	}
+	File* fileAtEnd = sourceVector.back();
+	sourceVector.pop_back();
+	filterFilesRec(sourceVector, destVector, extension);
+	if (fileAtEnd->getExtension() == extension) {
+		destVector.push_back(fileAtEnd);
 	}
 }
